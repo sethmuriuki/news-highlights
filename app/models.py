d00edfd3dac100e670eec0ab7f871469e5e05f99
first-name second-name”“git config --global user.email email@example.com” <sethkrm@gmail.com>
2017-12-09 16:59:18 +0300

    add class sources to models and create __init__ method to do defines its properties

---
+++ app/models.py
@@ -0,0 +1,15 @@
+class sources:
+    '''
+    source class  to to defines source objects
+    '''
+    
+    def __init__(self,id,name,description,url,category):
+        '''
+        __init__ method to define the properties of a source object
+        '''
+
+        self.id = id
+        self.name = name 
+        self.descrption = description
+        self.url = url 
+        self.category = category
