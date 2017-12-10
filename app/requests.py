cb389aec9a0a26c67e0360e9a7a5f90cea0dc0f8
first-name second-name”“git config --global user.email email@example.com” <sethkrm@gmail.com>
2017-12-10 19:31:40 +0300

    create function configure request

--- app/requests.py
+++ app/requests.py
@@ -4,3 +4,14 @@ from models import Sources, Highlights
 
 api_key = None
 
+def configure_request(app):
+
+    '''
+    function that takes in application instance and replace None variables to application configuration object
+    '''
+
+    global api_key,base_url
+    api_key = app.config['NEWS_API_KEY']
+
+
+    
