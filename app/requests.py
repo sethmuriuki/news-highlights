4f244ecf44ee75222ff125d2901eb2281866130c
first-name second-name”“git config --global user.email email@example.com” <sethkrm@gmail.com>
2018-01-03 16:58:49 +0300

    modify request

--- app/requests.py
+++ app/requests.py
@@ -18,7 +18,7 @@ def get_news_source(category):
     function that gets the json response to the url request
     '''
 
-    get_news_url = 'https://newsapi.org/v1/source'.format(category,api_key)
+    get_news_url = 'https://newsapi.org/v1/sources?category={}&apikey={}'.format(category,api_key)
     # get_news_url = 'https://newsapi.org/v2/sources?apiKey=937b10b0c16b4f408a44526714103037'
     with urllib.request.urlopen(get_news_url) as url:
         get_news_data = url.read()
